0595a99b15fa91119d13cd6362922eb96bdf19ef
Keith Lancaster <keithlancaster@unr.edu>
2021-09-07 14:30:08 -0700

    before refactor

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 
+
 class Cat {
 public:
+    // Cat() = default;
+//    Cat() {
+//        std::cout << "in the default constructor" << std::endl;
+//    }
+    Cat(const Cat& rhs){
+        //age = rhs.age;
+        std::cout << "in the copy constructor" << std::endl;
+    }
+//    Cat(int newAge = 0){
+//      age = newAge;
+//    }
+    Cat(int newAge = 0):age(newAge){ // initialization list
+    }
     void speak() {
         std::cout << "meow" << std::endl;
     }
+private:
+    int age;
+public:
+    int getAge() const {
+        return age;
+    }
+
+    void setAge(int newAge) {
+        age = newAge;
+    }
+
 };
 
 int main() {
-    Cat cat;
-    cat.speak();
+    Cat sarah(10);
+    Cat tim(1234);
+    Cat fred = tim;
+    std::cout << fred.getAge() << std::endl;
+    std::cout << tim.getAge() << std::endl;
+    tim.setAge(12);
+    tim.speak();
 }
